c2bf9f709c5eae493a4be7343c90a9d46eb6a065
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-22 16:10:42 +0530

    Table of Number n

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicCoreProgram
+{
+    class Poweroftwo
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int multiplier;
+            int result;
+
+            for(multiplier = 1; multiplier <= 10; multiplier++)
+            {
+                result = number * multiplier;
+                Console.WriteLine("{0} * {1} = {2}",number,multiplier,result);
+            }
+        }
+    }
+}
